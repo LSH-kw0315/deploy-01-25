f727335cf1e7ef38ffca0eb85a55898d69212236
LSH-kw0315 <tmdgjsdl1234@naver.com>
2025-01-13 17:18:12 +0900

    wow

--- src/main/java/com/ll/demo03/domain/home/home/controller/HomeController.java
+++ src/main/java/com/ll/demo03/domain/home/home/controller/HomeController.java
@@ -15,7 +15,7 @@ public class HomeController {
     @GetMapping("/")
     @ResponseBody
     public String showMain() {
-        return "Hello, World!!!!!, on " + siteName;
+        return "Hello, World!!!!!, on " + siteName+", 안녕하세요!";
     }
 
     @GetMapping("/secretKey")
